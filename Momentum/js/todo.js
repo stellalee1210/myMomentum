0afb355758349b7c0f651ee2771af3d1b853045a
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-13 16:41:11 +0900

    Initial Commit

---
+++ Momentum/js/todo.js
@@ -0,0 +1,62 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos"
+
+let toDos = []
+
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+    saveToDos();
+}
+
+
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    li.id = newToDo.id; 
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    
+    span.innerText = newToDo.text;
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+
+    //li 안에 span 넣기
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value ="";
+    const newToDoObj = {
+        text:newToDo,
+        id: Date.now(),
+    };
+
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if(savedTodos !== null){
+    const parsedToDos = JSON.parse(savedTodos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
